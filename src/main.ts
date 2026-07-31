9d45876257dce67914d62d42dbbf30fb49e0daab
David Magalhães <david200253@gmail.com>
2026-07-31 17:11:36 +0100

    Add loading screen

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { Assets } from './Assets.js';
 import { Engine } from './engine/Engine.js';
 import { Title } from './game/scenes/Title.js';
 
-const canvas = document.querySelector("#game") as HTMLCanvasElement;
-const engine = new Engine(canvas);
+const canvas = document.getElementById("game") as HTMLCanvasElement;
+const loadingScreen = document.getElementById("loading_screen") as HTMLImageElement;
 await Assets.load();
+loadingScreen.style.display = "none";
+canvas.style.display = "inline";
+const engine = new Engine(canvas);
 engine.start(Title);
